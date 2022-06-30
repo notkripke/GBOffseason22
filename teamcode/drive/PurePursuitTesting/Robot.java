a25c1e5323a21dcc42a44054f4350a7da60244bd
notkripkeHOME <cleary-ethan@el.gstboces.org>
2022-06-30 11:47:53 -0400

    Massive

--- teamcode/drive/PurePursuitTesting/Robot.java
+++ teamcode/drive/PurePursuitTesting/Robot.java
@@ -6,9 +6,9 @@ import org.firstinspires.ftc.teamcode.Teamcode.teamcode.drive.SampleMecanumDrive
 
 public class Robot {
 
-    public double robot_x_pos = 0;
-    public double robot_y_pos = 0;
-
+    public static double movement_x;
+    public static double movement_y;
+    public static double movement_turn;
 
 }
 
--- teamcode/drive/PurePursuitTesting/RobotMovement.java
+++ teamcode/drive/PurePursuitTesting/RobotMovement.java
@@ -1,16 +1,61 @@
 package org.firstinspires.ftc.teamcode.Teamcode.teamcode.drive.PurePursuitTesting;
 
-import static org.firstinspires.ftc.teamcode.Teamcode.teamcode.drive.PurePursuitTesting.MathFunctions.AngleWrap;
+import static org.firstinspires.ftc.teamcode.Teamcode.teamcode.drive.PurePursuitTesting.MathFunctions.*;
 
 import com.qualcomm.robotcore.util.Range;
+
 
 import org.firstinspires.ftc.teamcode.Teamcode.teamcode.drive.*;
 
 import org.firstinspires.ftc.teamcode.Teamcode.teamcode.drive.PurePursuitTesting.Robot;
 
 import org.firstinspires.ftc.teamcode.Teamcode.teamcode.drive.PurePursuitTesting.MathFunctions;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+
+import java.util.ArrayList;
 
 public class RobotMovement{
+
+/*
+    public static  void followCurve(ArrayList<CurvePoint> allPoints, double followAngle){
+
+        ArrayList<CurvePoint> pathExtended = (ArrayList<CurvePoint>) allPoints.clone();
+
+        CurvePoint followMe = getFollowPointPath(allPoints, new Point(robot_pos_x, robot_pos_y), allPoints.get(0).followDistance);
+
+        goToPosition(followMe.x, followMe.y, followMe.moveSpeed, followAngle, followMe.turnSpeed);
+    }
+
+    public static CurvePoint getFollowPointPath(ArrayList<CurvePoint> pathPoints, Point robotLocation, double followRadius){
+        CurvePoint followMe = new CurvePoint(pathPoints.get(0));
+
+        for (int i = 0; i < pathPoints.size() - 1; i ++){
+            CurvePoint startLine = pathPoints.get(1);
+            CurvePoint endLine = pathPoints.get(i + 1);
+
+            //ArrayList<Point> intersections = lineCircleIntersection(robotLocation, followRadius, startLine.toPoint(), endLine.toPoint());
+
+            /*double closestAngle = 10000000;
+
+            for (Point thisIntersection : intersections){
+                double angle = Math.atan2(thisIntersection.y - robot_pos_y, thisIntersection.x = robot_pos_x);
+                double deltaAngle = Math.abs(MyPosition.AngleWrap(angle = robot_pos_r));
+
+                if(deltaAngle < closestAngle){
+                    closestAngle = deltaAngle;
+                    followMe.setPoint(thisIntersection);
+                }
+            }
+
+        }
+        return  followMe;
+    }
+
+
+
+
+
 
     //to be fixed once ready for real trial. will be held in GorillabotsCentral-esque class
 
@@ -34,7 +79,7 @@ public class RobotMovement{
 
         double absoluteAngleToTarget = Math.atan2(y-robot_pos_y, x-robot_pos_x);
 
-        double relativeAngleToPoint = AngleWrap(absoluteAngleToTarget - (robot_pos_r - Math.toRadians(90)));  // for this example, 90 will be forwards
+        double relativeAngleToPoint = MyPosition.AngleWrap(absoluteAngleToTarget - (robot_pos_r - Math.toRadians(90)));  // for this example, 90 will be forwards
 
         double relativeXToPoint = Math.cos(relativeAngleToPoint) * distanceToTarget;
         double relativeYToPoint = Math.sin(relativeAngleToPoint) * distanceToTarget;
@@ -52,5 +97,5 @@ public class RobotMovement{
             power_r = 0;
         }
 
-    }
+    }*/
 }
